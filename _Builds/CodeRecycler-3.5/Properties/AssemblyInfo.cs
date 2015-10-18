9c82a7fe4db7b9296b07e2dfdba38e5425afe5d9
Ewen @ 8560wX <Ewen@CADbloke.com>
2015-10-18 20:18:19 +1100

    Split AssemblyInfo into a Common plus App-Specific info. BUG: Excluded because of Exclude.Contains(). Bad idea, huh?

--- _Builds/CodeRecycler-3.5/Properties/AssemblyInfo.cs
+++ _Builds/CodeRecycler-3.5/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeRecyclerGui")]
-[assembly: AssemblyDescription("Code Recycler Windows Forms App")]
-[assembly: AssemblyProduct("CodeRecyclerGui")]
+[assembly: AssemblyTitle("CodeRecycler")]
+[assembly: AssemblyDescription("Code Recycler for .NET 3.5")]
+[assembly: AssemblyProduct("CodeRecycler3.5")]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("396125c3-f024-463e-b512-93f9401152c4")]
 
